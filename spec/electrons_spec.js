7e153f28d2947dcd773edd7fc764a7ee3c72e0e6
Ceyda Birsu Aslan <ceydabirsu@MacBook-Air-von-Ceyda.local>
2026-05-25 22:01:46 +0200

    Third electron test case

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -6,8 +6,13 @@ describe('Electrons', function () {
     it('returns 6 for [1, 2, 3, 4, 5]', function () {
     expect(calculator.aroundTheCores([1, 2, 3, 4, 5])).toBe(6);
     });
-    
+
     it('returns 4 for [2, 2, 3, 3]', function () {
     expect(calculator.aroundTheCores([2, 2, 3, 3])).toBe(4);
     });
+
+    it('returns 2 for [6, 6, 4, 4, 1, 3]', function () {
+    expect(calculator.aroundTheCores([6, 6, 4, 4, 1, 3])).toBe(2);
+    });
+
 });
