bcb4f2b281ccaec3095be77ea26935d3f98c7c9c
Ceyda Birsu Aslan <ceydabirsu@MacBook-Air-von-Ceyda.local>
2026-05-25 22:01:13 +0200

    Second electron test case

--- spec/electrons_spec.js
+++ spec/electrons_spec.js
@@ -2,7 +2,12 @@ describe('Electrons', function () {
 	beforeEach(function() {
 		calculator = new ElectronsCalculator();
 	});
+
     it('returns 6 for [1, 2, 3, 4, 5]', function () {
     expect(calculator.aroundTheCores([1, 2, 3, 4, 5])).toBe(6);
-});
+    });
+    
+    it('returns 4 for [2, 2, 3, 3]', function () {
+    expect(calculator.aroundTheCores([2, 2, 3, 3])).toBe(4);
+    });
 });
